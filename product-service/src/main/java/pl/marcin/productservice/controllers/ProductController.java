7779715c9166e112079de7559d18e9dd3fec0d06
Marcin Prze <marprz26@gmail.com>
2021-03-09 16:11:18 +0100

    Add credit numbers attribute to get customers

--- product-service/src/main/java/pl/marcin/productservice/controllers/ProductController.java
+++ product-service/src/main/java/pl/marcin/productservice/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.marcin.productservice.models.Product;
 import pl.marcin.productservice.services.ProductService;
@@ -18,8 +19,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping(path = "GetProducts", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Product> getProducts() {
-        return productService.getAllProducts();
+    public List<Product> getProducts(@RequestBody List<String> creditNumbers) {
+        return productService.getAllProducts(creditNumbers);
     }
 
     @PostMapping(path = "CreateProduct", produces = MediaType.APPLICATION_JSON_VALUE)
--- product-service/src/main/java/pl/marcin/productservice/services/ProductService.java
+++ product-service/src/main/java/pl/marcin/productservice/services/ProductService.java
@@ -2,11 +2,14 @@ package pl.marcin.productservice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import pl.marcin.productservice.exceptions.ParameterValidationException;
 import pl.marcin.productservice.models.Product;
 import pl.marcin.productservice.repositories.ProductRepository;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
@@ -18,8 +21,14 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> getAllProducts() {
-        return (List<Product>) productRepository.findAll();
+    public List<Product> getAllProducts(List<String> creditNumbers) {
+        if(creditNumbers.isEmpty()) {
+            throw new ParameterValidationException();
+        }
+        return creditNumbers.stream()
+                .map(id -> productRepository.findById(id).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     public Product createProduct(Product product) {
