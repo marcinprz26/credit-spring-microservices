bd6e5259510b167190fb3ca2a43e433afcd92708
Marcin Prze <marprz26@gmail.com>
2021-03-09 10:44:43 +0100

    Add credit repository interface

---
+++ credit-service/src/main/java/pl/marcin/creditservice/reposotories/CreditRepository.java
@@ -0,0 +1,9 @@
+package pl.marcin.creditservice.reposotories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.marcin.creditservice.models.Credit;
+
+@Repository
+public interface CreditRepository extends CrudRepository<Credit, Long> {
+}
