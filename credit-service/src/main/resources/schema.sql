cc8b87094f926566aad788b2378d12fcb37171b3
Marcin Prze <marprz26@gmail.com>
2021-03-09 14:02:49 +0100

    Changed properties for services
    Extract separated database to each service
    Correct table generation

--- credit-service/src/main/resources/schema.sql
+++ credit-service/src/main/resources/schema.sql
@@ -2,25 +2,6 @@ CREATE TABLE IF NOT EXISTS credit (
     id numeric(20) NOT NULL,
     name varchar(150) NOT NULL,
 
-    constraint credit_pk primary key(id),
-);
-
-CREATE TABLE IF NOT EXISTS customer (
-    credit_id numeric(20) NOT NULL,
-    first_name varchar(150) NOT NULL,
-    last_name varchar(150) NOT NULL,
-    pesel integer NOT NULL,
-
-    constraint customer_pk primary key(credit_id),
-    constraint credit_fk foreign key(credit_id) references credit(id)
-);
-
-CREATE TABLE IF NOT EXISTS product (
-    credit_id numeric(20) NOT NULL,
-    name varchar(150) NOT NULL,
-    value numeric(2),
-
-    constraint product_pk primary key(credit_id),
-    constraint credit_fk foreign key(credit_id) references credit(id)
+    constraint credit_pk primary key(id)
 );
 
