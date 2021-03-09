e807c89890e86c3121dab7ee4ef18f9decbe6614
Marcin Prze <marprz26@gmail.com>
2021-03-09 16:12:08 +0100

    Add credit numbers attribute to get customers

--- customer-service/src/main/java/pl/marcin/customerservice/controllers/CustomerController.java
+++ customer-service/src/main/java/pl/marcin/customerservice/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.marcin.customerservice.models.Customer;
 import pl.marcin.customerservice.services.CustomerService;
@@ -18,8 +19,8 @@ public class CustomerController {
     private CustomerService customerService;
 
     @GetMapping(path = "GetCustomers", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Customer> getProducts() {
-        return customerService.getAllCustomers();
+    public List<Customer> getProducts(@RequestBody List<String> creditNumbers) {
+        return customerService.getAllCustomers(creditNumbers);
     }
 
     @PostMapping(path = "CreateCustomer", produces = MediaType.APPLICATION_JSON_VALUE)
--- customer-service/src/main/java/pl/marcin/customerservice/services/CustomerService.java
+++ customer-service/src/main/java/pl/marcin/customerservice/services/CustomerService.java
@@ -6,19 +6,29 @@ import pl.marcin.customerservice.exceptions.ParameterValidationException;
 import pl.marcin.customerservice.models.Customer;
 import pl.marcin.customerservice.repositories.CustomerRepository;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
 
-    private CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
     @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
-    public List<Customer> getAllCustomers() {
-        return (List<Customer>) customerRepository.findAll();
+    public List<Customer> getAllCustomers(List<String> creditNumbers) {
+        if(creditNumbers.isEmpty()) {
+            throw new ParameterValidationException();
+        }
+        return creditNumbers.stream()
+                .map(id -> customerRepository.findById(id).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 
     public Customer createCustomer(Customer product) {
