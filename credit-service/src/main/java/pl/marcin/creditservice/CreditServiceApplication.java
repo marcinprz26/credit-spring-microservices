98fcf45c0567863268ad20ff017162b3dd8f025d
Marcin Prze <marprz26@gmail.com>
2021-03-10 03:36:31 +0100

    Add eureka client to services

--- credit-service/src/main/java/pl/marcin/creditservice/CreditServiceApplication.java
+++ credit-service/src/main/java/pl/marcin/creditservice/CreditServiceApplication.java
@@ -2,10 +2,12 @@ package pl.marcin.creditservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CreditServiceApplication {
 
     public static void main(String[] args) {
